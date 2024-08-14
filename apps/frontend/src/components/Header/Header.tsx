2603302a27df0838126293ffb34f6ae3fc07e811
Andraz <69682837+andyv09@users.noreply.github.com>
2024-08-14 17:21:20 +0200

    feat: simple market page (#21)

--- apps/frontend/src/components/Header/Header.tsx
+++ apps/frontend/src/components/Header/Header.tsx
@@ -111,6 +111,11 @@ const Header: React.FC<IProps> = () => {
       link: ROUTES.FAUCET,
       icon: theme.images.icons.coins,
     },
+    {
+      name: 'Market',
+      link: ROUTES.MARKET,
+      icon: theme.images.icons.moon,
+    },
     // {
     //   name: 'Tutorials',
     //   link: ROUTES.TUTORIALS,
