e511badfe5e3dfc9eb088712ce5fdd8e37e9086a
Andraz <69682837+andyv09@users.noreply.github.com>
2024-08-22 13:09:36 +0200

    feat: frontend v2 faucet page (#28)
    Co-authored-by: martines3000 <domajnko.martin@gmail.com>

---
+++ apps/frontend-v2/src/components/DashboardView/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <div className="flex justify-between">
+      <div>Supplied Balance</div>
+      <div>Supply/borrow APR</div>
+      <div>Borrowed balance</div>
+    </div>
+  );
+};
