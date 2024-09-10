519dfa24d312ccc0bd8323ae1a9175805e026bbe
Andraz <69682837+andyv09@users.noreply.github.com>
2024-09-10 11:10:58 +0200

    feat: position summary (#46)
    Co-authored-by: martines3000 <domajnko.martin@gmail.com>

--- apps/frontend-v2/src/hooks/useUserLiquidationPoint.ts
+++ apps/frontend-v2/src/hooks/useUserLiquidationPoint.ts
@@ -1,13 +1,22 @@
-import { useMemo } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import BigNumber from 'bignumber.js';
 import { useUserCollateralUtilization } from './useUserCollateralUtilization';
 import { useUserCollateralValue } from './useUserCollateralValue';
 
 export const useUserLiquidationPoint = () => {
-  const collateralValue = useUserCollateralValue();
-  const userCollateralUtilization = useUserCollateralUtilization();
+  const { data: collateralValue } = useUserCollateralValue();
+  const { data: userCollateralUtilization } = useUserCollateralUtilization();
 
-  const userLiquidationPoint = useMemo(() => {
-    return collateralValue.times(userCollateralUtilization);
-  }, [collateralValue, userCollateralUtilization]);
-  return userLiquidationPoint;
+  return useQuery({
+    queryKey: [
+      'userLiquidationPoint',
+      collateralValue,
+      userCollateralUtilization,
+    ],
+    queryFn: async () => {
+      if (!collateralValue || !userCollateralUtilization) return BigNumber(0);
+      return collateralValue.times(userCollateralUtilization);
+    },
+    enabled: !!collateralValue && !!userCollateralUtilization,
+  });
 };
