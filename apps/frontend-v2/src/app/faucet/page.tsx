5fd2da774d5afc05ca95050be00005530875db7c
martines3000 <domajnko.martin@gmail.com>
2024-09-10 17:31:18 +0200

    feat: add basic metadata

--- apps/frontend-v2/src/app/faucet/page.tsx
+++ apps/frontend-v2/src/app/faucet/page.tsx
@@ -1,5 +1,11 @@
 import { FaucetView } from '@/components/FaucetView';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Faucet',
+};
+
 export default function Page() {
   return <FaucetView />;
 }
