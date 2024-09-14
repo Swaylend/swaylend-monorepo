b91dd8c0339bddbed907a1b0ce173e424e1416d4
martines3000 <domajnko.martin@gmail.com>
2024-09-14 20:43:22 +0200

    chore: remove testing code

--- apps/frontend-v2/src/components/DashboardView/index.tsx
+++ apps/frontend-v2/src/components/DashboardView/index.tsx
@@ -13,36 +13,6 @@ export const DashboardView = () => {
     <div className="pt-[33px] sm:pt-[55px] pb-[55px] sm:px-[40px] xl:px-[88px] flex flex-col w-full items-center justify-center">
       <Stats />
       <MarketSwitch />
-      <button
-        type="button"
-        onClick={() =>
-          TransactionSuccessToast({
-            transactionId:
-              '0x6aa991c67b9a3f034172fd57b301fa55585fdb1733e191daf4c2d1218ebe19af',
-          })
-        }
-      >
-        Open Toast
-      </button>
-      <button
-        type="button"
-        onClick={() =>
-          ErrorToast({ error: 'Error sending this or that babeeeyyyy' })
-        }
-      >
-        Open Toast Err
-      </button>
-      <button
-        type="button"
-        onClick={() =>
-          InfoToast({
-            title: 'Info!',
-            description: 'Error sending this or that babeeeyyyy',
-          })
-        }
-      >
-        Open Toast Err
-      </button>
       <AssetsTable />
       <InputDialog />
       <RedeemReferralDialog />
