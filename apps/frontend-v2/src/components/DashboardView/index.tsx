562269fb08b859da270ed48018da98f28be10801
andyv09 <andraz.vrecko@gmail.com>
2024-09-14 19:02:36 +0200

    feat: update toasts

--- apps/frontend-v2/src/components/DashboardView/index.tsx
+++ apps/frontend-v2/src/components/DashboardView/index.tsx
@@ -1,8 +1,8 @@
 'use client';
-import { useUserRole } from '@/hooks';
 import React from 'react';
 import { InputDialog } from '../InputDialog';
 import { RedeemReferralDialog } from '../RedeemReferralDialog';
+import { ErrorToast, InfoToast, TransactionSuccessToast } from '../Toasts';
 import { AssetsTable } from './AssetsTable';
 import { MarketSwitch } from './MarketSwitch';
 import { Stats } from './Stats';
@@ -12,6 +12,36 @@ export const DashboardView = () => {
     <div className="pt-[33px] sm:pt-[55px] pb-[55px] sm:px-[40px] xl:px-[88px] flex flex-col w-full items-center justify-center">
       <Stats />
       <MarketSwitch />
+      <button
+        type="button"
+        onClick={() =>
+          TransactionSuccessToast({
+            transactionId:
+              '0x6aa991c67b9a3f034172fd57b301fa55585fdb1733e191daf4c2d1218ebe19af',
+          })
+        }
+      >
+        Open Toast
+      </button>
+      <button
+        type="button"
+        onClick={() =>
+          ErrorToast({ error: 'Error sending this or that babeeeyyyy' })
+        }
+      >
+        Open Toast Err
+      </button>
+      <button
+        type="button"
+        onClick={() =>
+          InfoToast({
+            title: 'Info!',
+            description: 'Error sending this or that babeeeyyyy',
+          })
+        }
+      >
+        Open Toast Err
+      </button>
       <AssetsTable />
       <InputDialog />
       <RedeemReferralDialog />
