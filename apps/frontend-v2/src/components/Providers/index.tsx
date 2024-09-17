b3875b2137a232969d3ba15c5efefe74881699ed
martines3000 <domajnko.martin@gmail.com>
2024-09-17 19:32:04 +0200

    chore: only capture page visit and wallet connect

--- apps/frontend-v2/src/components/Providers/index.tsx
+++ apps/frontend-v2/src/components/Providers/index.tsx
@@ -60,6 +60,8 @@ export const Providers = ({ children }: { children: ReactNode }) => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
       person_profiles: 'always',
+      autocapture: false,
+      capture_pageview: true,
       loaded: (posthog) => {
         if (process.env.NODE_ENV === 'development') posthog.debug(); // debug mode in development
       },
