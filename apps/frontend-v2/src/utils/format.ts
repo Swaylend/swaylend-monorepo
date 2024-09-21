35a6cfa28b5458c50153e874f65e6a8ebb3c0247
tadejpodrekar <tadej.podrekar@gmail.com>
2024-09-21 20:13:17 +0200

    chore: add utils functions

---
+++ apps/frontend-v2/src/utils/format.ts
@@ -0,0 +1,25 @@
+export function formatCurrency(
+  value: number,
+  currency = 'USD',
+  locale = 'en-US'
+) {
+  const absValue = Math.abs(value);
+
+  if (absValue >= 1000000) {
+    const million = absValue / 1000000;
+    const formattedMillion = new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency: currency,
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(million);
+
+    return `${formattedMillion}M`;
+  }
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(value);
+}
--- apps/frontend-v2/src/utils/market.ts
+++ apps/frontend-v2/src/utils/market.ts
@@ -1,4 +1,5 @@
 import BigNumber from 'bignumber.js';
+import { BN, bn } from 'fuels';
 import { formatUnits } from './BigNumber';
 
 export function getBorrowApr(borrowRate: BigNumber | null | undefined) {
@@ -21,4 +22,75 @@ export function getSupplyApr(supplyRate: BigNumber | null | undefined) {
     .times(60)
     .times(100);
   return `${formatUnits(rate.times(coefficient), 18).toFormat(2)}%`;
+}
+
+export function calculateSupplyRate(
+  utilization: BigNumber | null | undefined,
+  marketConfiguration: any
+) {
+  if (marketConfiguration == null) return BigNumber(0);
+  if (utilization == null) return BigNumber(0);
+  const FACTOR_SCALE_18 = BigNumber(1).times(BigNumber(10).pow(18));
+  const supplyPerSecondInterestRateBase = BigNumber(
+    marketConfiguration.supplyPerSecondInterestRateBase.toString()
+  );
+  const supplyPerSecondInterestRateSlopeLow = BigNumber(
+    marketConfiguration.supplyPerSecondInterestRateBase.toString()
+  );
+  const supplyPerSecondInterestRateSlopeHigh = BigNumber(
+    marketConfiguration.supplyPerSecondInterestRateSlopeHigh.toString()
+  );
+  const supplyKink = BigNumber(marketConfiguration.supplyKink.toString());
+
+  if (utilization.lte(supplyKink)) {
+    return supplyPerSecondInterestRateBase.plus(
+      supplyPerSecondInterestRateSlopeLow.times(
+        utilization.div(FACTOR_SCALE_18)
+      )
+    );
+  }
+  return supplyPerSecondInterestRateBase
+    .plus(
+      supplyPerSecondInterestRateSlopeLow.times(supplyKink).div(FACTOR_SCALE_18)
+    )
+    .plus(
+      supplyPerSecondInterestRateSlopeHigh.times(
+        utilization.minus(supplyKink).div(FACTOR_SCALE_18)
+      )
+    );
+}
+
+export function calculateBorrowRate(
+  utilization: BigNumber | null | undefined,
+  marketConfiguration: any
+) {
+  if (marketConfiguration == null) return BigNumber(0);
+  if (utilization == null) return BigNumber(0);
+  const FACTOR_SCALE_18 = BigNumber(1).times(BigNumber(10).pow(18));
+  const borrowPerSecondInterestRateBase = BigNumber(
+    marketConfiguration.borrowPerSecondInterestRateBase.toString()
+  );
+  const borrowPerSecondInterestRateSlopeLow = BigNumber(
+    marketConfiguration.borrowPerSecondInterestRateSlopeLow.toString()
+  );
+  const borrowPerSecondInterestRateSlopeHigh = BigNumber(
+    marketConfiguration.borrowPerSecondInterestRateSlopeHigh.toString()
+  );
+  const borrowKink = BigNumber(marketConfiguration.borrowKink.toString());
+  if (utilization.lte(borrowKink)) {
+    return borrowPerSecondInterestRateBase.plus(
+      borrowPerSecondInterestRateSlopeLow.times(
+        utilization.div(FACTOR_SCALE_18)
+      )
+    );
+  }
+  return borrowPerSecondInterestRateBase
+    .plus(
+      borrowPerSecondInterestRateSlopeLow.times(borrowKink).div(FACTOR_SCALE_18)
+    )
+    .plus(
+      borrowPerSecondInterestRateSlopeHigh.times(
+        utilization.minus(borrowKink).div(FACTOR_SCALE_18)
+      )
+    );
 }
