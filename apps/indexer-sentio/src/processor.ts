e930adff00694a2980a7014a0cfcd30562115216
martines3000 <domajnko.martin@gmail.com>
2024-10-04 10:40:15 +0200

    chore: lint

--- apps/indexer-sentio/src/processor.ts
+++ apps/indexer-sentio/src/processor.ts
@@ -1,4 +1,5 @@
 import { GLOBAL_CONFIG } from '@sentio/runtime';
+import { BigDecimal } from '@sentio/sdk';
 import { FuelNetwork } from '@sentio/sdk/fuel';
 import { getPriceBySymbol } from '@sentio/sdk/utils';
 import dayjs from 'dayjs';
@@ -19,7 +20,6 @@ import {
   UserBasic,
 } from './schema/store.js';
 import { MarketProcessor } from './types/fuel/MarketProcessor.js';
-import { BigDecimal } from '@sentio/sdk';
 
 dayjs.extend(utc);
 
