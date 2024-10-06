8e974bda92f7e043519b5ef7132da206147602de
Urban Vidovič <urbanfoundit@gmail.com>
2024-10-06 22:05:26 +0200

    chore: add collaterals to setup devnet script
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- scripts/devnet/setup-devnet.sh
+++ scripts/devnet/setup-devnet.sh
@@ -4,8 +4,8 @@
 TOKEN_CONTRACT_ID="0xad56293ee3dc2ce81005d33c95ecbeb1549dbfb1adb242b81090dba390241067"
 SRC_20_CONTRACT_ID="0xbfc6b400716e93736953f1fdf956300e036abdaf2b41fb30e70fb813b87d4c34"
 ORACLE_CONTRACT_ID="0xc7b7ba72307fd1681017a6ed2e958629a87a2e48cb32dc2ed0e783918398c588"
-TARGET_CONTRACT_ID="0xee51fa53d17de68ce364a9bb61fd4544369c0e47b81a6dbfd6145e763b8b1828"
-PROXY_CONTRACT_ID="0x4f1814444f2e995803d88179476421f04f774b5c4c40956b0655b1e62b1c695d"
+TARGET_CONTRACT_ID="0x42fccc9904373da253b186c515ab52337128947a7f5a6f2916c92ea821320047"
+PROXY_CONTRACT_ID="0x42fccc9904373da253b186c515ab52337128947a7f5a6f2916c92ea821320047"
 BASE_TOKEN_PRICE_FEED="0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a"
 
 USDC_CONTRACT_ID="0xc75cf050b2b7495f504f7dc30a7aa45230e0373116fe0566bc44e2c84595f414"
@@ -19,7 +19,7 @@ UNI_CONTRACT_ID="0x69c6de7ebf99efd3ab5af9289a45fd9626ccbb61b012b35853b91c2287529
 # fi
 
 cd ../../
-forc deploy --node-url http://localhost:4000 \
+forc deploy --node-url http://localhost:4000/v1/graphql \
     --salt market:0x0000000000000000000000000000000000000000000000000000000000000001 \
     --salt token:0x0000000000000000000000000000000000000000000000000000000000000002 \
     --salt pyth-mock:0x0000000000000000000000000000000000000000000000000000000000000003 \
@@ -33,3 +33,6 @@ cargo run --bin mint_tokens -- --config-path ./configs/devnet_usdc_mock_config.j
 
 # activate market
 cargo run --bin activate_market -- --config-path ./configs/devnet_usdc_mock_config.json
+
+# add collaterals
+cargo run --bin update_collateral_assets -- --config-path ./configs/devnet_usdc_mock_config.json
