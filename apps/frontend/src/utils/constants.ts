87e865b8c5f3dfba8d673fd749bb25afedcbf962
martines3000 <domajnko.martin@gmail.com>
2024-10-07 16:30:12 +0200

    chore: add more asset logos

--- apps/frontend/src/utils/constants.ts
+++ apps/frontend/src/utils/constants.ts
@@ -2,10 +2,15 @@ import fuel from '/public/icons/fuel-logo.svg?url';
 import btc from '/public/tokens/bitcoin.svg?url';
 import bnb from '/public/tokens/bnb.svg?url';
 import eth from '/public/tokens/ethereum.svg?url';
+import ezeth from '/public/tokens/ezeth.svg?url';
+import pzeth from '/public/tokens/pzeth.svg?url';
+import sdai from '/public/tokens/sdai.svg?url';
 import sway from '/public/tokens/sway.svg?url';
 import uni from '/public/tokens/uni.svg?url';
 import usdc from '/public/tokens/usdc.svg?url';
 import usdt from '/public/tokens/usdt.svg?url';
+import weeth from '/public/tokens/weeth.svg?url';
+import wsteth from '/public/tokens/wsteth.svg?url';
 
 // Contract addresses
 export type MarketConfiguration = {
@@ -35,6 +40,13 @@ export const SYMBOL_TO_ICON: Record<string, any> = {
   BTC: btc,
   UNI: uni,
   BNB: bnb,
+  ezETH: ezeth,
+  pzETH: pzeth,
+  sDAI: sdai,
+  weETH: weeth,
+  wstETH: wsteth,
+  SolvBTC: '/tokens/solvbtc.png',
+  'SolvBTC.BBN': '/tokens/solvbtcbbn.png',
 };
 
 export const SYMBOL_TO_NAME: Record<string, string> = {
@@ -43,5 +55,12 @@ export const SYMBOL_TO_NAME: Record<string, string> = {
   USDT: 'USDT',
   BTC: 'Bitcoin',
   UNI: 'Uniswap',
-  BNB: 'Binance Coin',
+  BNB: 'BNB',
+  ezETH: 'ezETH',
+  pzETH: 'pzETH',
+  sDAI: 'sDAI',
+  weETH: 'weETH',
+  wstETH: 'wstETH',
+  SolvBTC: 'SolvBTC',
+  'SolvBTC.BBN': 'SolvBTC.BBN',
 };
