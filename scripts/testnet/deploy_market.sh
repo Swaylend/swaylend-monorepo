9db2b3cfc8bcd42f74f99a835be46d89c707cf73
Urban <urbanfoundit@gmail.com>
2024-10-08 14:27:19 +0200

    chore: update root README.md and remove old testnet scripts (#138)
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- scripts/testnet/deploy_market.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ORACLE_ADDRESS=$1
-TOKEN_CONTRACT_ADDRESS=$2
-
-cd ../../contracts/market
-SECRET=0x0331479c5df9d693c52bd5036cd3c21ef913dfae507231f526ce0b17ebbad6cc RPC=https://testnet.fuel.network/v1/graphql ORACLE_ADDRESS=$ORACLE_ADDRESS TOKEN_CONTRACT_ADDRESS=$TOKEN_CONTRACT_ADDRESS RANDOM=true cargo test --release scripts::deploy::deploy -- --nocapture
