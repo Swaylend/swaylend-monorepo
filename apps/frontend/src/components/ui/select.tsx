d869b10a2b82a36671fa37e5cee8947be9d80f4c
martines3000 <domajnko.martin@gmail.com>
2024-10-08 18:01:59 +0200

    fix: safari market switch issues

--- apps/frontend/src/components/ui/select.tsx
+++ apps/frontend/src/components/ui/select.tsx
@@ -19,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      'flex min-h-10 overflow-visible w-full border-0 ring-0 outline-none items-center justify-between rounded-md bg-background px-3 py-2 text-sm placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+      'flex min-h-10 h-10 overflow-visible w-full border-0 ring-0 outline-none items-center justify-between rounded-md bg-background px-3 py-2 text-sm placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 [&>span]:overflow-visible',
       className
     )}
     {...props}
