340714a4ee254aa203f523d4d5d065b451dd12b8
martines3000 <domajnko.martin@gmail.com>
2024-10-09 13:40:15 +0200

    chore: try catch posthog init

--- apps/frontend/src/components/Providers/index.tsx
+++ apps/frontend/src/components/Providers/index.tsx
@@ -92,17 +92,21 @@ const wagmiConfig = createConfig({
 });
 
 if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'development') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
-    person_profiles: 'always',
-    autocapture: false,
-    capture_pageview: true,
-    capture_pageleave: false,
+  try {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+      person_profiles: 'always',
+      autocapture: false,
+      capture_pageview: true,
+      capture_pageleave: false,
 
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.debug(); // debug mode in development
-    },
-  });
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug(); // debug mode in development
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const Providers = ({ children }: { children: ReactNode }) => {
