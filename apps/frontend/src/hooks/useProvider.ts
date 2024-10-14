76136d2554efa99379c00d6e3dad1d27baeb0ed4
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-10-14 17:32:25 -0300

    fix: infinite fetching while waiting for provider

--- apps/frontend/src/hooks/useProvider.ts
+++ apps/frontend/src/hooks/useProvider.ts
@@ -1,17 +1,10 @@
 import { appConfig } from '@/configs';
-import { useProvider as useFuelProvider } from '@fuels/react';
-import { Provider } from 'fuels';
-import { useEffect, useState } from 'react';
+import { useNetworkProvider } from '@fuels/react';
 
 export const useProvider = () => {
-  const [customProvider, setCustomProvider] = useState<Provider | null>(null);
-  const { provider } = useFuelProvider();
+  const networkQueryData = useNetworkProvider({
+    networkUrl: appConfig.client.fuelNodeUrl,
+  });
 
-  useEffect(() => {
-    Provider.create(appConfig.client.fuelNodeUrl)
-      .then((_provider) => setCustomProvider(_provider))
-      .catch((error) => console.error(`Error creating provider: ${error}`));
-  }, []);
-
-  return { provider: provider ?? customProvider };
+  return { provider: networkQueryData.networkProvider };
 };
--- apps/frontend/src/utils/provider.ts
+++
@@ -1,7 +0,0 @@
-import { appConfig } from '@/configs';
-import { Provider } from 'fuels';
-
-export const initProvider = async () => {
-  console.log('Initializing Fuel provider...');
-  return await Provider.create(appConfig.client.fuelNodeUrl);
-};
