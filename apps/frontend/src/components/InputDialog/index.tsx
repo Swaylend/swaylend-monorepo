7a3e4b3d51bf057d3d949df62868b2725c5a69dc
Andraz <69682837+andyv09@users.noreply.github.com>
2024-10-14 11:41:15 +0200

    fix: error msg & repay amount error (#177)

--- apps/frontend/src/components/InputDialog/index.tsx
+++ apps/frontend/src/components/InputDialog/index.tsx
@@ -34,7 +34,7 @@ import {
   selectTokenAmount,
   useMarketStore,
 } from '@/stores';
-import { formatUnits, getFormattedNumber } from '@/utils';
+import { SYMBOL_TO_NAME, formatUnits, getFormattedNumber } from '@/utils';
 import { useAccount, useIsConnected } from '@fuels/react';
 import * as VisuallyHidden from '@radix-ui/react-visually-hidden';
 import BigNumber from 'bignumber.js';
@@ -428,17 +428,41 @@ export const InputDialog = () => {
 
       // Balance more than user borrowed
       if (userSupplyBorrow.borrowed.eq(0)) return 'You have no debt';
-      const userBorrowed =
-        formatUnits(
-          userSupplyBorrow.borrowed,
-          marketConfiguration?.baseTokenDecimals
-        ).plus(
-          BigNumber(0.02).div(
+
+      const userBorrowed = formatUnits(
+        userSupplyBorrow.borrowed,
+        marketConfiguration?.baseTokenDecimals
+      );
+
+      const userBorrowedModified =
+        userBorrowed.plus(
+          BigNumber(0.002).div(
             priceData?.prices[marketConfiguration.baseToken.bits] ?? 1
           )
         ) ?? BigNumber(0);
 
-      if (tokenAmount.gt(userBorrowed))
+      const userBorrowedModifiedRepay =
+        userBorrowed
+          .plus(
+            BigNumber(0.001).div(
+              priceData?.prices[marketConfiguration.baseToken.bits] ?? 1
+            )
+          )
+          .decimalPlaces(marketConfiguration?.baseTokenDecimals) ??
+        BigNumber(0);
+
+      const minOpenPositionValue = BigNumber(
+        marketConfiguration.baseBorrowMin.toString()
+      ).dividedBy(BigNumber(10).pow(marketConfiguration.baseTokenDecimals));
+
+      if (
+        userBorrowedModifiedRepay.minus(tokenAmount).lt(minOpenPositionValue) &&
+        userBorrowedModifiedRepay.minus(tokenAmount).gt(0)
+      ) {
+        return `Your position cannot be less than ${minOpenPositionValue.toFixed()} ${SYMBOL_TO_NAME[appConfig.assets[marketConfiguration.baseToken.bits]]}. Please repay whole position or leave a position of at least ${minOpenPositionValue.toFixed()} ${SYMBOL_TO_NAME[appConfig.assets[marketConfiguration.baseToken.bits]]} open.`;
+      }
+
+      if (tokenAmount.gt(userBorrowedModified))
         return 'You are trying to repay more than your debt';
     }
 
