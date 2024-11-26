fd68e91ab6e93569b9afb82f9b620360c9f63ce8
martines3000 <domajnko.martin@gmail.com>
2024-11-26 09:42:29 +0100

    chore: remove old hook

--- apps/frontend/src/components/Navbar/Points.tsx
+++ apps/frontend/src/components/Navbar/Points.tsx
@@ -5,7 +5,6 @@ import {
 } from '@/components/ui/popover';
 import { useUser } from '@/hooks';
 import { useFuelPoints } from '@/hooks/useFuelPoints';
-import { useSwayPoints } from '@/hooks/useSwaylendPoints';
 import { cn } from '@/lib/utils';
 import { useIsConnected } from '@fuels/react';
 import { Trophy } from 'lucide-react';
@@ -22,24 +21,12 @@ export const Points = () => {
   const isHover = useHover(hoverRef);
 
   const [isManualOpen, setIsManualOpen] = useState(false);
-  // const setOpen = useReferralModalStore(selectReferralModalSetOpen);
 
   const { data: user } = useUser();
-
-  // const [isCopied, setIsCopied] = useState(false);
 
   const { isConnected } = useIsConnected();
 
   const { data: fuelPoints } = useFuelPoints();
-  const { data: swaylendPoints } = useSwayPoints();
-
-  // const handleCopy = async (value: string) => {
-  //   setIsCopied(true);
-  //   await navigator.clipboard.writeText(value);
-  //   setTimeout(() => {
-  //     setIsCopied(false);
-  //   }, 1000);
-  // };
 
   return (
     <Popover open={isHover || isManualOpen}>
@@ -48,9 +35,6 @@ export const Points = () => {
         ref={hoverRef}
         onClick={() => setIsManualOpen(true)}
       >
-        {/* <div className="text-yellow-400 sm:px-3 sm:py-1.5 max-sm:p-1 flex items-center gap-x-1 rounded-full border-[3px] border-yellow-400">
-          <span className="max-sm:hidden">Coming Soon</span>
-          </div> */}
         <Image alt="points-icon" width={40} height={40} src={POINTS} />
       </PopoverTrigger>
       <PopoverContent
@@ -81,35 +65,6 @@ export const Points = () => {
             Points Leaderboard
           </Button>
         </Link>
-        {/* <div className="w-full">
-          <Button
-            className={cn('w-full flex gap-x-2', isLoading && 'animate-pulse')}
-            variant="tertiary-card"
-            disabled={isPending}
-            onMouseDown={async () => {
-              if (isError || !user) return await refetch();
-              await handleCopy(user.inviteCode);
-            }}
-          >
-            {!isLoading && !isError && (
-              <>
-                <Copy className="w-5 h-5" />
-                {isCopied ? 'Copied' : 'Copy referral code'}
-              </>
-            )}
-            {isLoading && <Loader className="w-5 h-5 animate-spin" />}
-            {!isPending && isError && 'Refresh'}
-          </Button>
-          {user?.redeemedInviteCode === false && (
-            <Button
-              className="w-full flex gap-x-2 mt-2"
-              variant="tertiary-card"
-              onMouseDown={() => setOpen(true)}
-            >
-              Redeem referral code
-            </Button>
-          )}
-        </div> */}
       </PopoverContent>
     </Popover>
   );
